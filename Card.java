004111355af30094f4dc4b40145cb206298ac0fa
DanaeMorrison <156708590+DanaeMorrison@users.noreply.github.com>
2024-03-04 10:55:54 -0400

    Updated versions

--- Card.java
+++ Card.java
@@ -9,19 +9,21 @@ public class Card {
     private int Ztokens;
     private int winks;
     private int jumpPos;
+    private int spiderMove;
 
     private Card(boolean bothConditions, boolean isNightmare) {
         this.bothConditions = bothConditions;
         this.isNightmare = isNightmare;
     }
 
-    public Card (boolean bothConditions, boolean isNightmare, ArrayList<Integer> moves, int Ztokens, int winks, int jumpPos) {
+    public Card (boolean bothConditions, boolean isNightmare, ArrayList<Integer> moves, int Ztokens, int winks, int jumpPos, int spiderMove) {
         this.bothConditions = bothConditions;
         this.isNightmare = isNightmare;
         this.moves = moves;
         this.Ztokens = Ztokens;
         this.winks = winks;
         this.jumpPos = jumpPos;
+        this.spiderMove = spiderMove;
     }
     
     public void setMoves(ArrayList<Integer> moves) {
@@ -40,6 +42,10 @@ public class Card {
         this.jumpPos = jumpPos;
     }
 
+    public void setSpiderMove(int spiderMove) {
+        this.spiderMove = spiderMove;
+    }
+
     public ArrayList<Integer> getMoves() {
         return moves;
     }
@@ -55,6 +61,11 @@ public class Card {
     public int getJumpPos() {
         return jumpPos;
     }
+
+    public int getSpiderMove() {
+        return spiderMove;
+    }
+
     public static class Builder {
         private final boolean bothConditions;
         private final boolean isNightmare;
@@ -63,6 +74,7 @@ public class Card {
         private int Ztokens = 0;
         private int winks = 0;
         private int jumpPos = 0;
+        private int spiderMove = 0;
 
         public Builder (boolean bothConditions, boolean isNightmare) {
             this.bothConditions = bothConditions;
@@ -89,12 +101,18 @@ public class Card {
             return this;
         }
 
+        public Builder withSpiderMove(int spiderMove) {
+            this.spiderMove = spiderMove;
+            return this;
+        }
+
         public Card build() {
             Card card = new Card(bothConditions, isNightmare);
             card.setMoves(moves);
             card.setZtokens(Ztokens);
             card.setWinks(winks);
             card.setJumpPos(jumpPos);
+            card.setSpiderMove(spiderMove);
             return card;
         }
     }
--- DeckGenerator.java
+++ DeckGenerator.java
@@ -1,7 +1,12 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import javax.smartcardio.Card;
+
 public class DeckGenerator {
+    private static final int NIGHTMARELEN = 10;
+    private static final int PLAYERTWOLEN = 30;
+    
     private Deck deck;
     private int players;
     private int nightmare;
@@ -9,33 +14,36 @@ public class DeckGenerator {
     //Solo and 2 player information
     private ArrayList<Boolean> isNightmarePlayer2 = new ArrayList<>();
     private ArrayList<Boolean> bothConditionsPlayer2 = new ArrayList<>();
-    //private ArrayList<Integer> moves = new ArrayList<Integer>(Arrays.asList());
-    private int[] movesList = {{2}, {3}, {4}, {4}, {1, 2}, {1, 2}, {6}, {5}, {4}, {4}, {4}, {4}, {6}, {1}, {4}, {6}, {3}, {3}, {3}, {2}, {2}, 
+    private int[] movesListPlayerTwo = {{2}, {3}, {4}, {4}, {1, 2}, {1, 2}, {6}, {5}, {4}, {4}, {4}, {4}, {6}, {1}, {4}, {6}, {3}, {3}, {3}, {2}, {2}, 
                                     {3}, {3}, {1, 5}, {1, 5}, {7}, {7}, {3}, {5}, {1}};
+    private ArrayList<Integer> movesPlayer2 = new ArrayList<Integer>(Arrays.asList(movesListPlayerTwo));
     private ArrayList<Integer> ZtokensPlayer2 = new ArrayList<Integer>(Arrays.asList(2, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 
                                                                                 0, 1, 1, 1, 2, 2, 1, 1, 0, 0, 0, 0, 1, 0, 1));
     private ArrayList<Integer> winksPlayer2 = new ArrayList<Integer>(Arrays.asList(0, 0, 0, 0, 0, 0, 3, 2, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 
                                                                                 0, 0, 0, 0, 0, 0, 0, 2, 0));
-    private ArrayList<Integer> jumpPosPlayer2 = new ArrayList<Integer>();
+    //private ArrayList<Integer> jumpPosPlayer2 = new ArrayList<Integer>();
+    //private ArrayList<Integer> spiderMovePlayer2 = new ArrayList<Integer>();
 
     //Nightmare information
     private ArrayList<Boolean> isNightmareReal = new ArrayList<>();
     private ArrayList<Boolean> bothConditionsFalse = new ArrayList<>();
-    private ArrayList<Integer> ZtokensFalse = new ArrayList<Integer>();
-    private ArrayList<Integer> winksFalse = new ArrayList<Integer>();
+    //private ArrayList<Integer> ZtokensZero = new ArrayList<Integer>();
+    //private ArrayList<Integer> winksZero = new ArrayList<Integer>();
 
-    //add a no moves
-
+    //private ArrayList<Integer> movesZero = new ArrayList<Integer>();
+    //private ArrayList<Integer> jumpPosZero = new ArrayList<Integer>();
+    //private ArrayList<Integer> spiderMoveZero = new ArrayList<Integer>();
+    
     //Wolf
-    private ArrayList<Integer> jumpPosFalse = new ArrayList<Integer>();
-    //add moves
+    private ArrayList<Integer> movesWolf = new ArrayList<Integer>(Arrays.asList(1, 1, 1, 1, 2, 2, 2, 2, 10, 10));
+    
 
     //Bump
-    private ArrayList<Integer> jumpPosBump = new ArrayList<Integer>();
+    private ArrayList<Integer> jumpPosBump = new ArrayList<Integer>(Arrays.asList(3, 3, 2, 2, 2, 2, 1, -1, -1, -2));
 
 
     //Spider
-    private ArrayList<Integer> jumpPosSpider = new ArrayList<Integer>();
+    private ArrayList<Integer> spiderMove = new ArrayList<Integer>(Arrays.asList(3, 3, 3, 3, 3, 3, 2, 2, 2, 2));
     
     public DeckGenerator (Deck deck, int players, int nightmare) {
         this.deck = deck;
@@ -45,7 +53,7 @@ public class DeckGenerator {
 
     //Solo and 2 player information
     public ArrayList<Boolean> fillFalse(ArrayList<Boolean> playerCards) {
-        for (int i = 0; i < 30; i++) {
+        for (int i = 0; i < PLAYERTWOLEN; i++) {
             playerCards.add(false);
         }
         return playerCards;
@@ -57,8 +65,8 @@ public class DeckGenerator {
         }
     }
     */
-    public ArrayList<Integer> fillZero(ArrayList<Integer> cards) {
-        for (int i = 0; i < 30; i++) {
+    public ArrayList<Integer> fillZeroPlayerTwo(ArrayList<Integer> cards) {
+        for (int i = 0; i < PLAYERTWOLEN; i++) {
             cards.add(0);
         }
         return cards;
@@ -66,21 +74,21 @@ public class DeckGenerator {
 
     //Nightmare information
     public ArrayList<Boolean> fillTrue(ArrayList<Boolean> nightmareCards) {
-        for (int i = 0; i < 10; i++) {
-            nightmareCards.add(false);
+        for (int i = 0; i < NIGHTMARELEN; i++) {
+            nightmareCards.add(true);
         }
         return nightmareCards;
     }
 
     public ArrayList<Boolean> fillFalseNightmare(ArrayList<Boolean> cards) {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < NIGHTMARELEN; i++) {
             cards.add(false);
         }
         return cards;
     }
 
     public ArrayList<Integer> fillZeroNightmare(ArrayList<Integer> cards) {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < NIGHTMARELEN; i++) {
             cards.add(0);
         }
         return cards;
@@ -88,7 +96,51 @@ public class DeckGenerator {
 
 
     public void makeDeck() {
-        //for i
+        //private ArrayList<Boolean> nightmarePlayer2 = new ArrayList<>();
+        isNightmarePlayer2 = fillFalse(isNightmarePlayer2);
+        bothConditionsPlayer2 = fillFalse(bothConditionsPlayer2);
+        //jumpPosPlayer2 = fillZeroPlayerTwo(jumpPosPlayer2);
+        //spiderMovePlayer2 = fillZeroPlayerTwo(spiderMovePlayer2);
+
+        isNightmareReal = fillTrue(isNightmareReal);
+        bothConditionsFalse = fillFalse(bothConditionsFalse);
+
+        for (int i = 0; i < PLAYERTWOLEN; i++) {
+            Card newCard = new Card.Builder(isNightmarePlayer2.get(i), bothConditionsPlayer2.get(i))
+                .withMoves(movesPlayerTwo.get(i))
+                .withZtokens(ZtokensPlayer2.get(i))
+                .withWinks(winksPlayer2.get(i))
+                .build();
+            deck.add(newCard);
+        }
+
+        if (nightmare == 1) {
+            for (int i = 0; i < NIGHTMARELEN; i++) {
+                Card newCard = new Card.Builder(isNightmareReal.get(i), bothConditionsFalse.get(i))
+                    .withMoves(movesWolf.get(i))
+                    .build();
+                deck.add(newCard);
+            }
+        }
+
+        if (nightmare == 2) {
+            for (int i = 0; i < NIGHTMARELEN; i++) {
+                Card newCard = new Card.Builder(isNightmareReal.get(i), bothConditionsFalse.get(i))
+                    .withJumpPos(jumpPosBump.get(i))
+                    .build();
+                deck.add(newCard);
+            }
+        }
+
+        if (nightmare == 3) {
+            for (int i = 0; i < NIGHTMARELEN; i++) {
+                Card newCard = new Card.Builder(isNightmareReal.get(i), bothConditionsFalse.get(i))
+                    .withSpiderMove(spiderMove.get(i))
+                    .build();
+                deck.add(newCard);
+            }
+        }
+
         // isn't just one loop needed to check values for a given index for one card?
     }
 
