667997b39f457c8fbc03501e6848858119e5362e
Julien Ouellette <jmouellette@mta.ca>
2024-03-06 11:23:12 -0400

    stuff

--- Deck.java
+++ Deck.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Deck {
-    private ArrayList<CardInterface> deck;
+    private ArrayList<Card> deck;
     
     public Deck(){
         deck = new ArrayList<>();
@@ -12,13 +12,13 @@ public class Deck {
         Collections.shuffle(deck);
     }
 
-    public CardInterface takeCard(){
-        CardInterface card = deck.get(0);
+    public Card takeCard(){
+        Card card = deck.get(0);
         deck.remove(0);
         return card;
     }
 
-    public void addCard(CardInterface card){
+    public void add(Card card){
         deck.add(card);
     }
 }
--- Player.java
+++ Player.java
@@ -1,21 +1,40 @@
+import java.util.ArrayList;
+
 public class Player{
     private String name;
+    private int orderPosition;
+    private ArrayList<Card> hand;
     private BoardInterface board;
     private boolean isScared;
     private int winks;
     private Score scoreboard;
 
-    public Player(String name){
+    public Player(String name, int orderPosition){
         BoardFactory factory = new BoardFactory();
         board = factory.createBoard("Player");
+
+        hand = new ArrayList<>();
         this.name = name;
+        this.orderPosition = orderPosition;
         winks = 0;
         scoreboard = new Score(this);
         setBrave();
     }
 
+    public int getOrderPosition(){
+        return orderPosition;
+    }
+
     public Score getScoreboard(){
         return scoreboard;
+    }
+
+    public ArrayList<Card> getHand(){
+        return hand;
+    }
+
+    public void setHand(ArrayList<Card> inHand){
+        hand = inHand;
     }
 
     public String getName(){
