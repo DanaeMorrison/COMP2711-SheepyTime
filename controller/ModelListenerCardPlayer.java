bd21a60bb66324eeb5236baf026603e18bd60b60
DanaeMorrison <156708590+DanaeMorrison@users.noreply.github.com>
2024-03-31 00:06:47 -0300

    Delete controller directory

--- controller/ModelListenerCardPlayer.java
+++
@@ -1,6 +0,0 @@
-package controller;
-
-public interface ModelListenerCardPlayer {
-    public void onRequestDisplayCardOptions (int[] validCardOptions);
-    public int onRequestAskAbility(int[] validCardOptions);
-}
--- controller/ModelListenerRacingPhase.java
+++
@@ -1,7 +0,0 @@
-package controller;
-
-public interface ModelListenerRacing {
-    public void onRequestPrintCard(Card card, int nightmare);
-    public int onRequestCardChoice(int[] validCardOptions);
-    //public 
-}
